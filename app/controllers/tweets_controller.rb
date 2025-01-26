b0a16b21669d3cd8af007a0b2bb36080468d2825
i-shintaro <shintaroiwashita7857@gmail.com>
2025-01-26 20:19:28 +0900

    保存、削除、更新

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,6 +1,47 @@
 class TweetsController < ApplicationController
+  # コントローラーで定義されたアクションが実行される前に、共通の処理を行う
+  before_action :set_tweet, only: [:edit, :show]
   def index
     #@tweetsのインスタンス変数ににTweetテーブルに入っているレコードを代入している
     @tweets = Tweet.all
+  end
+
+  def new
+    #@tweetのインスタンス変数にTweetモデルのnewメソッドを用いて新しいインスタンスを生成している
+    @tweet = Tweet.new
+  end
+
+  def create
+    Tweet.create(tweet_params)
+    redirect_to '/'
+  end
+
+  def destroy
+# paramsでルーティングから送られてきたidを使用して、Tweetモデルを使ってデータベースから該当するツイートオブジェクトを取得する
+    tweet = Tweet.find(params[:id])
+# tweet.destroyで、取得したツイートのレコードがデータベースから削除されます。
+    tweet.destroy
+    redirect_to root_path
+  end
+
+  def edit
+  end
+
+  def update
+    tweet = Tweet.find(params[:id])
+    tweet.update(tweet_params)
+    redirect_to root_path
+  end
+
+  def show
+  end
+
+  private
+  def tweet_params
+    params.require(:tweet).permit(:name, :image, :text)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
   end
 end
