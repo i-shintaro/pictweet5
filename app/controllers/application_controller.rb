6143a45682c78736c6a13ff1048fbbc3270692e4
i-shintaro <shintaroiwashita7857@gmail.com>
2025-01-30 11:29:01 +0900

    deviseビュー追加

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  # ifオプション（if: :devise_controller?）もしdeviseに関するコントローラーの処理であれば、そのときだけconfigure_permitted_parametersメソッドを実行
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+
+  private
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+  end
 end
